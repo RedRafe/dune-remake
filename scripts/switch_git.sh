c0fd436b8de175bfde7f9e49bd176d5ef3117b3b
RedRafe <gabri97.gt+redrafe@gmail.com>
2023-01-15 17:05:36 +0100

    init folder

---
+++ scripts/switch_git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --local user.name "RedRafe"
+git config --local user.email "gabri97.gt+redrafe@gmail.com"
+git config --local credential.username "RedRafe"
+git config --local --list
